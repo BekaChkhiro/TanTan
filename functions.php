7934be1e34536b9e6003a867f7c395f01c470318
BekaChkhiro <bekachkhirodze1@gmail.com>
2024-12-13 12:47:07 +0400

    functions updated

--- functions.php
+++ functions.php
@@ -23,3 +23,21 @@ function custom_theme_scripts() {
     wp_enqueue_script( 'custom-js', get_template_directory_uri() . '/assets/js/custom.js', array('jquery'), '1.0.0', true );
 }
 add_action( 'wp_enqueue_scripts', 'custom_theme_scripts' );
+
+// Include all necessary PHP files
+include_once get_template_directory() . '/footer.php';
+include_once get_template_directory() . '/front-page.php';
+include_once get_template_directory() . '/header.php';
+include_once get_template_directory() . '/index.php';
+include_once get_template_directory() . '/page.php';
+include_once get_template_directory() . '/sidebar.php';
+
+// Include all PHP files from template-parts directory
+foreach (glob(get_template_directory() . '/template-parts/*.php') as $file) {
+    include_once $file;
+}
+
+// Include all PHP files from woocommerce directory
+foreach (glob(get_template_directory() . '/woocommerce/*.php') as $file) {
+    include_once $file;
+}
